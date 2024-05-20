ce1d3679ce94729578406327d218bce14cf24cfb
Subhan F. <mahersubhan578@gmail.com>
2024-05-20 20:53:34 +0500

    Code from Slide

--- Code/Q1.py
+++ Code/Q1.py
@@ -1,1 +1,12 @@
-print("Hello, world!")
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+import os
+
+pwd = os.getcwd()
+print(pwd)
+
+data = pd.read_csv('posal.csv')
+print(data.shape)
+print(data)
+#data.head()
