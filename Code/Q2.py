a4091b3d9c859d09841f87b63868fd38d0aa729a
Subhan F. <mahersubhan578@gmail.com>
2024-05-29 16:51:01 +0500

    File Management

--- Code/Q2.py
+++ Code/Q2.py
@@ -21,7 +21,7 @@ class LogisticRegressionScratch:
     def __init__(self, learning_rate=0.01, epochs=10000):
         self.learning_rate = learning_rate
         self.epochs = epochs
-        self.weights = None
+        self.thetas = None
         self.bias = None
     
     def sigmoid(self, z):
@@ -29,21 +29,21 @@ class LogisticRegressionScratch:
     
     def fit(self, X, y):
         n_samples, n_features = X.shape
-        self.weights = np.zeros(n_features)
+        self.thetas = np.zeros(n_features)
         self.bias = 0
         
-        for _ in range(self.epochs):
-            linear_model = np.dot(X, self.weights) + self.bias
+        for i in range(self.epochs):
+            linear_model = np.dot(X, self.thetas) + self.bias
             y_predicted = self.sigmoid(linear_model)
             
             dw = (1 / n_samples) * np.dot(X.T, (y_predicted - y))
             db = (1 / n_samples) * np.sum(y_predicted - y)
             
-            self.weights -= self.learning_rate * dw
+            self.thetas -= self.learning_rate * dw
             self.bias -= self.learning_rate * db
     
     def predict(self, X):
-        linear_model = np.dot(X, self.weights) + self.bias
+        linear_model = np.dot(X, self.thetas) + self.bias
         y_predicted = self.sigmoid(linear_model)
         y_predicted_cls = [1 if i > 0.5 else 0 for i in y_predicted]
         return np.array(y_predicted_cls)
